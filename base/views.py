9a44eb28e1367f4ddc074383daeb3a4e61bdf04a
Samson Wanendeya <nateniel18@gmail.com>
2024-08-20 15:48:15 +0300

    added room count for each topic and also can be able to filter rooms by topic name, name and description

--- base/views.py
+++ base/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Room, Topic
 from .forms import RoomForm
+from django.db.models import Q  # this is provided by django to help with and | or searches bassing on multiple params
 
 # Create your views here.
 
@@ -9,11 +10,18 @@ from .forms import RoomForm
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
 
-    rooms = Room.objects.filter(topic__name__icontains=q) # topic__name is used to filter the rooms based on the topic name,
-                                                          # __icontains is used to filter the rooms based on the topic name and is case insensitive
-    topics = Topic.objects.all()
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+                                ) # topic__name is used to filter the rooms based on the topic name,
+                                  # __icontains is used to filter the rooms based on the topic name and is case insensitive
+                                #   the Q is used to combine the filters to search for the rooms based on the topic name, name and description
 
-    context = {'rooms':rooms, 'topics':topics}
+    topics = Topic.objects.all()
+    room_count = rooms.count()
+
+    context = {'rooms':rooms, 'topics':topics, 'room_count':room_count}
     return render(request, 'base/home.html',context)
 def room(request,pk):
     room = Room.objects.get(id=pk)
